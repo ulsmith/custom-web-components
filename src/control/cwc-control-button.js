9d2bcf08faac1fb975689f81717a2f1bae92ab1f
Paul Smith <p@ulsmith.net>
2020-04-03 08:13:32 +0100

    removed contexts... not needed, these can be done by css external to this component

--- src/control/cwc-control-button.js
+++ src/control/cwc-control-button.js
@@ -15,6 +15,7 @@ import { CustomHTMLElement, html, ifDefined } from '../../../custom-web-componen
  * @attribute {Flag} disabled Add to disabled the button
  *
  * @style_variable --cwc-control-button--border-radius
+ * @style_variable --cwc-control-button--box-shadow
  * @style_variable --cwc-control-button--height
  * @style_variable --cwc-control-button--width
  * @style_variable --cwc-control-button--line-height
@@ -28,17 +29,11 @@ import { CustomHTMLElement, html, ifDefined } from '../../../custom-web-componen
  * @style_variable --cwc-control-button--outline
  *
  * @style_variable --cwc-control-button--background--hover
- * @style_variable --cwc-control-button--box-shadow--focus
+ * @style_variable --cwc-control-button--background--focus
  * @style_variable --cwc-control-button--background--active
+ * @style_variable --cwc-control-button--box-shadow--hover
+ * @style_variable --cwc-control-button--box-shadow--focus
  * @style_variable --cwc-control-button--box-shadow--active
- * 
- * @style_variable --cwc-control-button--[context]--background
- * @style_variable --cwc-control-button--[context]--color
- * @style_variable --cwc-control-button--[context]--border
- * @style_variable --cwc-control-button--[context]--background--hover
- * @style_variable --cwc-control-button--[context]--box-shadow--focus
- * @style_variable --cwc-control-button--[context]--background--active
- * @style_variable --cwc-control-button--[context]--box-shadow--active
  *
  * @slot root Single root slot to show contents of button
  * 
@@ -72,6 +67,7 @@ class CWCControlButton extends CustomHTMLElement {
 					font-family: var(--cwc-control-button--font-family, inherit);
 					cursor: var(--cwc-control-button--cursor, default);
 					outline: var(--cwc-control-button--outline, none);
+					box-shadow: var(--cwc-control-button--box-shadow, none);
 					display: inline-block;
 					pointer-events: none; 
 					-webkit-touch-callout: none;
@@ -81,35 +77,9 @@ class CWCControlButton extends CustomHTMLElement {
 					user-select: none;
 				}
 
-				:host(:hover) .cwc-control-button { background: var(--cwc-control-button--background--hover, #ddd); }
-				:host(:focus) .cwc-control-button { box-shadow: var(--cwc-control-button--box-shadow--focus, 0 0 0 1px grey); }
-				:host(:active) .cwc-control-button { background: var(--cwc-control-button--background--active, #bbb); box-shadow: var(--cwc-control-button--box-shadow--active, 0 0 0 2px grey); }
-				
-				:host([context="primary"]) .cwc-control-button { background: var(--cwc-control-button--primary--background, blue); color: var(--cwc-control-button--primary--color, white); fill: var(--cwc-control-button--primary--color, white); border: var(--cwc-control-button--primary--border, 1px solid transparent); }
-				:host([context="primary"]:hover) .cwc-control-button { background: var(--cwc-control-button--primary--background--hover, darkblue); }			
-				:host([context="primary"]:focus) .cwc-control-button { margin: 0; box-shadow: var(--cwc-control-button--primary--box-shadow--focus, 0 0 0 1px lightblue); }
-				:host([context="primary"]:active) .cwc-control-button { margin: 0; background: var(--cwc-control-button--primary--background--active, blue); box-shadow: var(--cwc-control-button--primary--box-shadow--active, 0 0 0 2px lightblue); }
-				
-				:host([context="secondary"]) .cwc-control-button { background: var(--cwc-control-button--secondary--background, grey); color: var(--cwc-control-button--secondary--color, white); fill: var(--cwc-control-button--secondary--color, white); border: var(--cwc-control-button--secondary--border, 1px solid transparent); }
-				:host([context="secondary"]:hover) .cwc-control-button { background: var(--cwc-control-button--secondary--background--hover, darkgrey); }			
-				:host([context="secondary"]:focus) .cwc-control-button { margin: 0; box-shadow: var(--cwc-control-button--secondary--box-shadow--focus, 0 0 0 1px lightgrey); }
-				:host([context="secondary"]:active) .cwc-control-button { margin: 0; background: var(--cwc-control-button--secondary--background--active, grey); box-shadow: var(--cwc-control-button--secondary--box-shadow--active, 0 0 0 2px lightgrey); }
-
-				:host([context="success"]) .cwc-control-button { background: var(--cwc-control-button--success--background, green); color: var(--cwc-control-button--success--color, white); fill: var(--cwc-control-button--success--color, white); border: var(--cwc-control-button--success--border, 1px solid transparent); }
-				:host([context="success"]:hover) .cwc-control-button { background: var(--cwc-control-button--success--background--hover, darkgreen); }
-				:host([context="success"]:focus) .cwc-control-button { margin: 0; box-shadow: var(--cwc-control-button--success--box-shadow--focus, 0 0 0 1px lightgreen); }
-				:host([context="success"]:active) .cwc-control-button { margin: 0; background: var(--cwc-control-button--success--background--active, darkgreen); box-shadow: var(--cwc-control-button--success--box-shadow--active, 0 0 0 2px lightgreen); }
-
-				:host([context="warning"]) .cwc-control-button { background: var(--cwc-control-button--warning--background, orange); color: var(--cwc-control-button--warning--color, white); fill: var(--cwc-control-button--warning--color, white); border: var(--cwc-control-button--warning--border, 1px solid transparent); }
-				:host([context="warning"]:hover) .cwc-control-button { background: var(--cwc-control-button--warning--background--hover, darkorange); }
-				:host([context="warning"]:focus) .cwc-control-button { margin: 0; box-shadow: var(--cwc-control-button--warning--box-shadow--focus, 0 0 0 1px #f5c875); }
-				:host([context="warning"]:active) .cwc-control-button { margin: 0; background: var(--cwc-control-button--warning--background--active, darkorange); box-shadow: var(--cwc-control-button--warning--box-shadow--active, 0 0 0 #f5c875); }
-
-				:host([context="danger"]) .cwc-control-button { background: var(--cwc-control-button--danger--background, red); color: var(--cwc-control-button--danger--color, white); fill: var(--cwc-control-button--danger--color, white); border: var(--cwc-control-button--danger--border, 1px solid transparent); }
-				:host([context="danger"]:hover) .cwc-control-button { background: var(--cwc-control-button--danger--background--hover, darkred); }
-				:host([context="danger"]:focus) .cwc-control-button { margin: 0; box-shadow: var(--cwc-control-button--danger--box-shadow--focus, 0 0 0 1px #f98a8a); }
-				:host([context="danger"]:active) .cwc-control-button { margin: 0; background: var(--cwc-control-button--danger--background--active, darkred); box-shadow: var(--cwc-control-button--danger--box-shadow--active, 0 0 0 2px #f98a8a); }
-
+				:host(:hover) .cwc-control-button { background: var(--cwc-control-button--background--hover, #ddd); box-shadow: var(--cwc-control-button--box-shadow--hover, none); }
+				:host(:focus) .cwc-control-button { background: var(--cwc-control-button--background--focus, #ddd); box-shadow: var(--cwc-control-button--box-shadow--focus, 0 0 0 1px grey); }
+				:host(:active) .cwc-control-button { background: var(--cwc-control-button--background--active, #bbb); box-shadow: var(--cwc-control-button--box-shadow--active, 0 0 0 2px grey); }			
 				:host([disabled]) .cwc-control-button { pointer-events: none; cursor: not-allowed; opacity: var(--cwc-control-button--disabled--opacity, 0.6); }
 			</style>
 
