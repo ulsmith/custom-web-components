ff14f8450611df873fc9c798ddd1f4abfcd527a4
Paul Smith <p@ulsmith.net>
2020-04-03 07:48:26 +0100

    made validate test internal if no value set

--- src/control/cwc-control-box.js
+++ src/control/cwc-control-box.js
@@ -219,6 +219,7 @@ class CWCControlBox extends CustomHTMLElement {
      * @param {Event} ev Any event that kicks the function
 	 */
 	validate(value) {
+		value = value !== undefined ? value : this.value;
 		this.invalid = false;
 		this.invalid = this.hasAttribute('required') ? (!value || value.length < 1 ? true : false) : (!value || value.length < 1 ? false : this.invalid);
 
