206a81f7fc539d8ef519295e340b263a2c488d3c
Paul Smith <p@ulsmith.net>
2020-04-13 22:08:16 +0100

    chnges to add in styling overrides

--- src/overlay/cwc-overlay-authentication.js
+++ src/overlay/cwc-overlay-authentication.js
@@ -27,6 +27,107 @@ import '../icon/material/cwc-icon-material-general.js';
  * @attribute {String} system-version The version to output in the overlays as a footer
  * @attribute {String} api-url The URL of the API used before the routes above, such as http://my-app.com
  * 
+ * @style_variable --cwc-overlay-authentication--transition
+ * @style_variable --cwc-overlay-authentication--z-index
+ * 
+ * @style_variable --cwc-overlay-authentication--backdrop--background
+ * @style_variable --cwc-overlay-authentication--backdrop--opacity
+ * @style_variable --cwc-overlay-authentication--backdrop--z-index
+ * 
+ * @style_variable --cwc-overlay-authentication--box--background
+ * @style_variable --cwc-overlay-authentication--box--border-radius
+ * @style_variable --cwc-overlay-authentication--box--box-shadow
+ * @style_variable --cwc-overlay-authentication--box--color
+ * @style_variable --cwc-overlay-authentication--box--left
+ * @style_variable --cwc-overlay-authentication--box--max-width
+ * @style_variable --cwc-overlay-authentication--box--top
+ * @style_variable --cwc-overlay-authentication--box--transform
+ * @style_variable --cwc-overlay-authentication--box--width
+ * @style_variable --cwc-overlay-authentication--box--z-index
+ * 
+ * @style_variable --cwc-overlay-authentication--box-header--background
+ * @style_variable --cwc-overlay-authentication--box-header--border-radius
+ * @style_variable --cwc-overlay-authentication--box-header--color
+ * @style_variable --cwc-overlay-authentication--box-header--font-weight
+ * @style_variable --cwc-overlay-authentication--box-header--font-size
+ * @style_variable --cwc-overlay-authentication--box-header--height
+ * @style_variable --cwc-overlay-authentication--box-header--line-height
+ * @style_variable --cwc-overlay-authentication--box-header--margin
+ * @style_variable --cwc-overlay-authentication--box-header--padding
+ * @style_variable --cwc-overlay-authentication--box-header--text-transform
+ * @style_variable --cwc-overlay-authentication--box-header--text-align
+ * 
+ * @style_variable --cwc-overlay-authentication--box-main--background
+ * @style_variable --cwc-overlay-authentication--box-main--color
+ * @style_variable --cwc-overlay-authentication--box-main--margin
+ * @style_variable --cwc-overlay-authentication--box-main--padding
+ * @style_variable --cwc-overlay-authentication--box-main--input--border
+ * @style_variable --cwc-overlay-authentication--box-main--input--border-radius
+ * @style_variable --cwc-overlay-authentication--box-main--input--font-size
+ * @style_variable --cwc-overlay-authentication--box-main--input--margin
+ * @style_variable --cwc-overlay-authentication--box-main--input--padding
+ * @style_variable --cwc-overlay-authentication--box-main--input--invalid--background
+ * @style_variable --cwc-overlay-authentication--box-main--input--invalid--border
+ * @style_variable --cwc-overlay-authentication--box-main--button--background
+ * @style_variable --cwc-overlay-authentication--box-main--button--border
+ * @style_variable --cwc-overlay-authentication--box-main--button--bottom
+ * @style_variable --cwc-overlay-authentication--box-main--button--color
+ * @style_variable --cwc-overlay-authentication--box-main--button--height
+ * @style_variable --cwc-overlay-authentication--box-main--button--padding
+ * @style_variable --cwc-overlay-authentication--box-main--button--position
+ * @style_variable --cwc-overlay-authentication--box-main--button--right
+ * 
+ * @style_variable --cwc-overlay-authentication--box-message--font-size
+ * @style_variable --cwc-overlay-authentication--box-message--margin
+ * @style_variable --cwc-overlay-authentication--box-message--padding
+ * @style_variable --cwc-overlay-authentication--box-message--text-align
+ * 
+ * @style_variable --cwc-overlay-authentication--box-message-icon--height
+ * @style_variable --cwc-overlay-authentication--box-message-icon--margin
+ * @style_variable --cwc-overlay-authentication--box-message-icon--padding
+ * @style_variable --cwc-overlay-authentication--box-message-icon--width
+ * 
+ * @style_variable --cwc-overlay-authentication--box-message-danger--background
+ * @style_variable --cwc-overlay-authentication--box-message-danger--fill
+ * @style_variable --cwc-overlay-authentication--box-message-danger--padding
+ * 
+ * @style_variable --cwc-overlay-authentication--box-message-success--background
+ * @style_variable --cwc-overlay-authentication--box-message-success--fill
+ * @style_variable --cwc-overlay-authentication--box-message-success--padding
+ * 
+ * @style_variable --cwc-overlay-authentication--box-footer--background
+ * @style_variable --cwc-overlay-authentication--box-footer--border-radius
+ * @style_variable --cwc-overlay-authentication--box-footer--height
+ * @style_variable --cwc-overlay-authentication--box-footer--color
+ * @style_variable --cwc-overlay-authentication--box-footer--color--hover
+ * @style_variable --cwc-overlay-authentication--box-footer--cursor
+ * @style_variable --cwc-overlay-authentication--box-footer--font-size
+ * @style_variable --cwc-overlay-authentication--box-footer--line-height
+ * @style_variable --cwc-overlay-authentication--box-footer--padding
+ * @style_variable --cwc-overlay-authentication--box-footer--text-decoration
+ * 
+ * @style_variable --cwc-overlay-authentication--bumpf--bottom
+ * @style_variable --cwc-overlay-authentication--bumpf--color
+ * @style_variable --cwc-overlay-authentication--bumpf--font-size
+ * @style_variable --cwc-overlay-authentication--bumpf--left
+ * 
+ * @style_variable --cwc-overlay-authentication--bumpf-version--float
+ * 
+ * @style_variable --cwc-overlay-authentication--auth--background
+ * @style_variable --cwc-overlay-authentication--auth--border-radius
+ * @style_variable --cwc-overlay-authentication--auth--color
+ * 
+ * @style_variable --cwc-overlay-authentication--auth-title--color
+ * @style_variable --cwc-overlay-authentication--auth-title--font-size
+ * @style_variable --cwc-overlay-authentication--auth-title--padding
+ * @style_variable --cwc-overlay-authentication--auth-title--font-weight
+ *
+ * @style_variable --cwc-overlay-authentication--auth-icon--background
+ * @style_variable --cwc-overlay-authentication--auth-icon--border-radius
+ * @style_variable --cwc-overlay-authentication--auth-icon--fill
+ * @style_variable --cwc-overlay-authentication--auth-icon--height
+ * @style_variable --cwc-overlay-authentication--auth-icon--width
+ * 
  * @example HTML (requires REST API with specific endpoints)
  *	<cwc-overlay-authentication
  *		reset-route="reset"
@@ -75,8 +176,8 @@ class CWCOverlayAuthentication extends CustomHTMLElement {
 					left: 0;
 					width: 100%;
 					height: 100%;
-					transition: opacity 400ms ease-in-out;
-					z-index: 10000;
+					transition: opacity var(--cwc-overlay-authentication--transition-duration, 400ms) ease-in-out;
+					z-index: var(--cwc-overlay-authentication--z-index, 10000);
 				}
 
 				.backdrop { 
@@ -85,9 +186,9 @@ class CWCOverlayAuthentication extends CustomHTMLElement {
 					left: 0;
 					width: 100%;
 					height: 100%;
-					background: #444;
-					z-index: 10010; 
-					opacity: 0.995;
+					background: var(--cwc-overlay-authentication--backdrop--background, white);
+					z-index: var(--cwc-overlay-authentication--backdrop--z-index, 10010); 
+					opacity: var(--cwc-overlay-authentication--backdrop--opacity, 0.95);
 				}
 
 				.wrapper { 
@@ -96,69 +197,78 @@ class CWCOverlayAuthentication extends CustomHTMLElement {
 					left: 0;
 					width: 100%;
 					height: 100%; 
-					z-index: 10020; 
+					z-index: var(--cwc-overlay-authentication--box--z-index, 10020); 
 					background: transparent;
 				}
 
 				.box {
 					position: fixed;
-					top: 50%;
-					left: 50%;
-					width: 90%;
-					max-width: 300px;
-					transform: translate(-50%, -50%);
-					background: white;
-					-webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-					-moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-					box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+					top: var(--cwc-overlay-authentication--box--top, 50%);
+					left: var(--cwc-overlay-authentication--box--left, 50%);
+					width: var(--cwc-overlay-authentication--box--width, 90%);
+					max-width: var(--cwc-overlay-authentication--box--max-width, 300px);
+					transform: var(--cwc-overlay-authentication--box--transform, translate(-50%, -50%));
+					background: var(--cwc-overlay-authentication--box--background, white);
+					color: var(--cwc-overlay-authentication--box--color, #222);
+					-webkit-box-shadow: var(--cwc-overlay-authentication--box--box-shadow, 0px 0px 5px 0px rgba(0,0,0,0.75));
+					-moz-box-shadow: var(--cwc-overlay-authentication--box--box-shadow, 0px 0px 5px 0px rgba(0,0,0,0.75));
+					box-shadow: var(--cwc-overlay-authentication--box--box-shadow, 0px 0px 5px 0px rgba(0,0,0,0.75));
+					border-radius: var(--cwc-overlay-authentication--box--border-radius, 0px);
 				}
 
-				.box.auth { border-radius: 50px; width: auto; }
-				.header { height: 50px; width: 100%; background: #eee; }
+				.header { 
+					width: 100%; 
+					height: var(--cwc-overlay-authentication--box-header--height, 50px);
+					line-height: var(--cwc-overlay-authentication--box-header--line-height, 50px);
+					background: var(--cwc-overlay-authentication--box-header--background, #eee);
+					color: var(--cwc-overlay-authentication--box-header--color, #eee);
+					border-radius: var(--cwc-overlay-authentication--box-header--border-radius, 0px);
+				}
 				
 				.header > * {
-					font-weight: normal;
-					font-size: 20px;
-					text-transform: uppercase;
-					margin: 0;
-					padding: 10px;
-					line-height: 30px;
-					height: 30px;
-					color: #777;
-					text-align: center;
-				}
-
-				.message cwc-icon-material-general {
-					height: 16px;
-					width: 16px;
-					padding: 0px;
-					margin: 3px;
+					font-weight: var(--cwc-overlay-authentication--box-header--font-weight, normal);
+					font-size: var(--cwc-overlay-authentication--box-header--font-size, 20px);
+					text-transform: var(--cwc-overlay-authentication--box-header--text-transform, uppercase);
+					margin: var(--cwc-overlay-authentication--box-header--margin, 0);
+					padding: var(--cwc-overlay-authentication--box-header--padding, 10px);
+					line-height: var(--cwc-overlay-authentication--box-header--line-height, 30px);
+					height: var(--cwc-overlay-authentication--box-header--height, 30px);
+					color: var(--cwc-overlay-authentication--box-header--color, #777);
+					text-align: var(--cwc-overlay-authentication--box-header--text-align, center);
 				}
 				
-				.main { margin: 0px; padding: 0px; background: white; }
-				.main > * { margin: 0px; padding: 0px; color: #444; }
+				.main { 
+					margin: var(--cwc-overlay-authentication--box-main--margin, 0px);
+					padding: var(--cwc-overlay-authentication--box-main--padding, 0px);
+					background: var(--cwc-overlay-authentication--box-main--background, white);
+					color: var(--cwc-overlay-authentication--box-main--color, #444);
+				}
 
 				.main input {
 					width: calc(100% - 20px);
-					padding: 8px;
+					padding: var(--cwc-overlay-authentication--box-main--input--padding, 8px);
 					box-sizing: border-box;
-					font-size: 16px;
-					margin: 10px;
+					font-size: var(--cwc-overlay-authentication--box-main--input--font-size, 16px);
+					margin: var(--cwc-overlay-authentication--box-main--input--margin, 10px);
 					display: block;
-					border: 1px solid grey;
+					border: var(--cwc-overlay-authentication--box-main--input--border, 1px solid grey);
+					border-radius: var(--cwc-overlay-authentication--box-main--input--border-radius, 0);
 				}
 
-				.main input[invalid] { border: 1px solid red; }
+				.main input[invalid] { 
+					border: var(--cwc-overlay-authentication--box-main--input--invalid--border, 1px solid red);
+					background: var(--cwc-overlay-authentication--box-main--input--invalid--background, transparent);
+				}
 
 				.main button {
-					padding: 0 10px;
-					position: absolute;
-					right: 10px;
-					bottom: 10px;
-					height: 30px;
-					border: darkblue;
-					background: blue;
-					color: white;
+					padding: var(--cwc-overlay-authentication--box-main--button--padding, 0 10px);
+					position: var(--cwc-overlay-authentication--box-main--button--position, absolute);
+					right: var(--cwc-overlay-authentication--box-main--button--right, 10px);
+					bottom: var(--cwc-overlay-authentication--box-main--button--bottom, 10px);
+					height: var(--cwc-overlay-authentication--box-main--button--height, 30px);
+					border: var(--cwc-overlay-authentication--box-main--button--border, darkblue);
+					background: var(--cwc-overlay-authentication--box-main--button--background, blue);
+					color: var(--cwc-overlay-authentication--box-main--button--color, white);
 				}
 
 				.main button[disabled] {
@@ -170,71 +280,94 @@ class CWCOverlayAuthentication extends CustomHTMLElement {
 
 				.message  {
 					vertical-align: middle;
-					text-align: center;
-					font-size: 15px;
+					text-align: var(--cwc-overlay-authentication--box-message--text-align, center);
+					font-size: var(--cwc-overlay-authentication--box-message--font-size, 15px);
 					display: none;
-					margin: 10px;
-					padding: 5px;
+					margin: var(--cwc-overlay-authentication--box-message--margin, 10px);
+					padding: var(--cwc-overlay-authentication--box-message--padding, 5px);
+				}
+
+				.message cwc-icon-material-general {
+					height: var(--cwc-overlay-authentication--box-message-icon--height, 16px);
+					width: var(--cwc-overlay-authentication--box-message-icon--width, 16px);
+					padding: var(--cwc-overlay-authentication--box-message-icon--padding, 0px);
+					margin: var(--cwc-overlay-authentication--box-message-icon--margin, 3px);
 				}
 
 				.message[show] { display: block; }
-				.message[type="danger"] { color: red; fill: red; background: #ffd7d7; }
-				.message[type="success"] { color: green; fill: green; background: #cae6ca; }
+				
+				.message[type="danger"] { 
+					color: var(--cwc-overlay-authentication--box-message-danger--padding, red); 
+					fill: var(--cwc-overlay-authentication--box-message-danger--fill, red);
+					background: var(--cwc-overlay-authentication--box-message-danger--background, #ffd7d7);
+				}
+
+				.message[type="success"] { 
+					color: var(--cwc-overlay-authentication--box-message-success--padding, green); 
+					fill: var(--cwc-overlay-authentication--box-message-success--fill, green); 
+					background: var(--cwc-overlay-authentication--box-message-success--background, #cae6ca);
+				}
 
 				.footer {
-					height: 50px;
+					height: var(--cwc-overlay-authentication--box-footer--height, 50px);
 					width: 100%;
-					padding: 10px;
-					background: #eee;
+					padding: var(--cwc-overlay-authentication--box-footer--padding, 10px);
+					background: var(--cwc-overlay-authentication--box-footer--background, #eee);
+					border-radius: var(--cwc-overlay-authentication--box-footer--border-radius, 0px);
 					box-sizing: border-box;
 				}
 
 				.footer span {
 					display: inline-block;
 					margin: 0;
-					line-height: 30px;
-					font-size: 16px;
-					text-decoration: none;
-					cursor: pointer;
-					color: blue;
+					line-height: var(--cwc-overlay-authentication--box-footer--line-height, 30px);
+					font-size: var(--cwc-overlay-authentication--box-footer--font-size, 16px);
+					text-decoration: var(--cwc-overlay-authentication--box-footer--text-decoration, none);
+					cursor: var(--cwc-overlay-authentication--box-footer--cursor, pointer);
+					color: var(--cwc-overlay-authentication--box-footer--color, blue);
 				}
 
-				.footer span.link:hover { color: #00006c; }
-				.footer .button { float: right; }
+				.footer span.link:hover { color: var(--cwc-overlay-authentication--box-footer--color--hover, #00006c); }
 
 				.bumpf {
 					position: absolute;
-					bottom: -22px;
-					left: 0px;
+					bottom: var(--cwc-overlay-authentication--bumpf--bottom, -22px);
+					left: var(--cwc-overlay-authentication--bumpf--left, 0px);
 					display: inline-block;
-					font-size: 16px;
-					color: white;
+					font-size: var(--cwc-overlay-authentication--bumpf--font-size, 16px);
+					color:  var(--cwc-overlay-authentication--bumpf--color, #222);
 					width: 100%;
 				}
 
-				.bumpf span { font-size: 16px; color: white; }
-				.bumpf span.version { float: right; }
+				.bumpf span.version { float: var(--cwc-overlay-authentication--bumpf-version--float, right); }
 
-				.auth-heading {
+				.box.auth { 
+					border-radius: var(--cwc-overlay-authentication--auth--border-radius, 50px);
+					background: var(--cwc-overlay-authentication--auth--background, var(--cwc-overlay-authentication--box--background, white));
+					color: var(--cwc-overlay-authentication--auth--color, var(--cwc-overlay-authentication--box--color, #222));
+					width: auto;
+				}
+
+				.auth-title {
 					margin: 0;
-					padding: 10px;
-					color: grey;
+					padding: var(--cwc-overlay-authentication--auth-title--padding, 10px);
+					color: var(--cwc-overlay-authentication--auth-title--color, grey);
 					text-align: center;
-					font-weight: normal;
-					font-size: 20px;
+					font-weight: var(--cwc-overlay-authentication--auth-title--font-weight, normal);
+					font-size: var(--cwc-overlay-authentication--auth-title--font-size, 20px);
 					display: inline-block; 
 					white-space: nowrap;
 				}
 				
-				.auth-heading cwc-icon-material-general {
-					background: grey;
-					fill: white;
-					width: 34px;
-					height: 34px;
+				.auth-title cwc-icon-material-general {
+					background: var(--cwc-overlay-authentication--auth-icon--background, grey);
+					fill: var(--cwc-overlay-authentication--auth-icon--fill, white);
+					width: var(--cwc-overlay-authentication--auth-icon--width, 34px);
+					height: var(--cwc-overlay-authentication--auth-icon--height, 34px);
 					vertical-align: middle;
 					transform: scale(1);
 					animation: pulse 1s infinite;
-					border-radius: 50px;
+					border-radius: var(--cwc-overlay-authentication--auth-icon--border-radius, 50px);
 				}
 
 				@keyframes pulse {
@@ -308,7 +441,7 @@ class CWCOverlayAuthentication extends CustomHTMLElement {
 						</div>
 					</div>
 					<div id="auth-box" class="box auth">
-						<h1 class="auth-heading" ?hidden="${this._action !== 'authenticate'}"><cwc-icon-material-general name="lock"></cwc-icon-material-general> Authenticating...</h1>
+						<h1 class="auth-title" ?hidden="${this._action !== 'authenticate'}"><cwc-icon-material-general name="lock"></cwc-icon-material-general> Authenticating...</h1>
 					</div>
 				</div>
 			</div>
